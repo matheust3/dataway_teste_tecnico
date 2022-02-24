fe022122a07d2eecd5a364fbc69793e1aa04887c
Matheus Toniolli <matheus.toniolli@hotmail.com>
2022-02-24 18:54:26 +0000

    feat: add deploy db endpoint if not test

--- src/main/singletons/prismaClient.ts
+++ src/main/singletons/prismaClient.ts
@@ -4,7 +4,14 @@ class Singleton {
   readonly obj: PrismaClient
 
   private constructor () {
-    this.obj = new PrismaClient()
+    this.obj = new PrismaClient({
+      datasources: {
+        db: {
+          url: process.env.NODE_ENV === 'test' ? 'mongodb://127.0.0.1:27017,127.0.0.1:27018,127.0.0.1:27019/dev_miner?replicaSet=rs0&readPreference=primary&ssl=false' : 'mongodb+srv://nightmoon:Ng7PAoBrkWNTBl1q@cluster0.e2b50.mongodb.net/nightmoon'
+        }
+
+      }
+    })
   }
 
   static getInstance (): Singleton {
